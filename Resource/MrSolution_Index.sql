dd1400b06f035abf3eae0096a8b4774fd5da3c43
Yawahang Rai <yawahangkoyu@gmail.com>
2021-07-28 04:47:27 -0700

    added resources

---
+++ Resource/MrSolution_Index.sql
@@ -0,0 +1,30 @@
+USE MRSolution;
+
+/*
+
+	EXEC utl.ApplicationSetup
+	EXEC utl.MR_ApplicationSetup
+
+*/
+
+SELECT *
+FROM   dbo.[User] AS u;
+
+SELECT * FROM dbo.ListItem AS li
+SELECT * FROM dbo.ListItemCategory AS lic
+
+SELECT *
+FROM   dbo.DataAccess AS da;
+
+
+SELECT *
+FROM   dbo.Navigation AS n;
+
+
+SELECT *
+FROM   dbo.NavigationAction AS na;
+ 
+SELECT *
+FROM   dbo.Application AS a;
+ 
+
